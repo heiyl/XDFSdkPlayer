2d470e586f8d751088bca67e179aee7cb220a802
heiyl <hyl_09@aliyun.com>
2017-06-19 18:57:22 +0800

    视频播放实现自动旋转屏幕的功能

--- VodPlayerLib/src/com/gensee/player/activity/PlayerActivity.java
+++ VodPlayerLib/src/com/gensee/player/activity/PlayerActivity.java
@@ -1,6 +1,7 @@
 package com.gensee.player.activity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,10 +10,12 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -57,6 +60,7 @@ import java.util.List;
 public class PlayerActivity extends FragmentActivity implements OnPlayListener, OnTabSelectListener, View.OnClickListener {
 
     private static final String TAG = "PlayerActivity";
+    MyOrientoinListener myOrientoinListener;
     private SharedPreferences preferences;
     private Player mPlayer;
     private GSVideoView mGSViedoView;
@@ -214,6 +218,14 @@ public class PlayerActivity extends FragmentActivity implements OnPlayListener, 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        myOrientoinListener = new MyOrientoinListener(this);
+        boolean autoRotateOn = (android.provider.Settings.System.getInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 0) == 1);
+        //检查系统是否开启自动旋转
+        if (autoRotateOn) {
+            myOrientoinListener.enable();
+        }
 
         setContentView(R.layout.xdf_player_activity);
         preferences = getPreferences(MODE_PRIVATE);
@@ -660,6 +672,8 @@ public class PlayerActivity extends FragmentActivity implements OnPlayListener, 
         stopLogService();
         releasePlayer();
         // onFinshAll();
+        //销毁时取消监听
+        myOrientoinListener.disable();
         super.onDestroy();
     }
 
@@ -1002,5 +1016,46 @@ public class PlayerActivity extends FragmentActivity implements OnPlayListener, 
     public void onThirdVote(String arg0) {
         // TODO Auto-generated method stub
 
+    }
+
+    /**
+     * 手机的旋转监听类
+     */
+    class MyOrientoinListener extends OrientationEventListener {
+
+        public MyOrientoinListener(Context context) {
+            super(context);
+        }
+
+        public MyOrientoinListener(Context context, int rate) {
+            super(context, rate);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            Log.d(TAG, "orention" + orientation);
+            int screenOrientation = getResources().getConfiguration().orientation;
+            if (((orientation >= 0) && (orientation < 45)) || (orientation > 315)) {//设置竖屏
+                if (screenOrientation != ActivityInfo.SCREEN_ORIENTATION_PORTRAIT && orientation != ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT) {
+                    Log.d(TAG, "设置竖屏");
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                }
+            } else if (orientation > 225 && orientation < 315) { //设置横屏
+                Log.d(TAG, "设置横屏");
+                if (screenOrientation != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+            } else if (orientation > 45 && orientation < 135) {// 设置反向横屏
+                Log.d(TAG, "反向横屏");
+                if (screenOrientation != ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                }
+            } /*else if (orientation > 135 && orientation < 225) {
+                Log.d(TAG, "反向竖屏");
+                if (screenOrientation != ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                }
+            }*/
+        }
     }
 }
--- VodPlayerLib/src/com/gensee/vod/activity/VodPlayerActivity.java
+++ VodPlayerLib/src/com/gensee/vod/activity/VodPlayerActivity.java
@@ -1,6 +1,7 @@
 package com.gensee.vod.activity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -8,10 +9,13 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -56,6 +60,8 @@ import java.util.List;
 public class VodPlayerActivity extends FragmentActivity implements OnClickListener, OnVodListener,SeekBar.OnSeekBarChangeListener,VODPlayer.OnVodPlayListener,OnTabSelectListener {
 
     private String TAG = "VodPlayerActivity";
+
+    MyOrientoinListener myOrientoinListener;
     private VodSite vodSite;
 
     //初始化的参数
@@ -126,6 +132,14 @@ public class VodPlayerActivity extends FragmentActivity implements OnClickListen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        myOrientoinListener = new MyOrientoinListener(this);
+        boolean autoRotateOn = (android.provider.Settings.System.getInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 0) == 1);
+        //检查系统是否开启自动旋转
+        if (autoRotateOn) {
+            myOrientoinListener.enable();
+        }
+
         setContentView(R.layout.xdf_voidplayer_activity);
         startLogService();
 
@@ -266,6 +280,8 @@ public class VodPlayerActivity extends FragmentActivity implements OnClickListen
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        //销毁时取消监听
+        myOrientoinListener.disable();
     }
 
     /********************* OnVodListener **************************/
@@ -934,6 +950,48 @@ public class VodPlayerActivity extends FragmentActivity implements OnClickListen
         params.width = ViewGroup.LayoutParams.MATCH_PARENT;
         params.height = DensityUtil.dip2px(this,281);
         gsVideoView.setLayoutParams(params);
+    }
+
+
+    /**
+     * 手机的旋转监听类
+     */
+    class MyOrientoinListener extends OrientationEventListener {
+
+        public MyOrientoinListener(Context context) {
+            super(context);
+        }
+
+        public MyOrientoinListener(Context context, int rate) {
+            super(context, rate);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            Log.d(TAG, "orention" + orientation);
+            int screenOrientation = getResources().getConfiguration().orientation;
+            if (((orientation >= 0) && (orientation < 45)) || (orientation > 315)) {//设置竖屏
+                if (screenOrientation != ActivityInfo.SCREEN_ORIENTATION_PORTRAIT && orientation != ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT) {
+                    Log.d(TAG, "设置竖屏");
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                }
+            } else if (orientation > 225 && orientation < 315) { //设置横屏
+                Log.d(TAG, "设置横屏");
+                if (screenOrientation != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+            } else if (orientation > 45 && orientation < 135) {// 设置反向横屏
+                Log.d(TAG, "反向横屏");
+                if (screenOrientation != ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                }
+            } /*else if (orientation > 135 && orientation < 225) {
+                Log.d(TAG, "反向竖屏");
+                if (screenOrientation != ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                }
+            }*/
+        }
     }
 
 
