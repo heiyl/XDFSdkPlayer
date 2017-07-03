98e6348c88858d25d9fa4fba565aeca099ee12fd
wuxu <wuxu5566@gmail.com>
2017-07-03 10:41:55 +0800

    rename to LivePlayer

--- VodPlayerLib/gen/com/gensee/playerdemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.gensee.playerdemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- rtmpResourceLib/gen/com/gensee/rtmpresourcelib/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.gensee.rtmpresourcelib;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
